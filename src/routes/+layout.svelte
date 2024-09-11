d4a48e757d05b7af14b540b06b1e3ffd852ab599
Alessio Grespi <56415370+AlessioGrespi@users.noreply.github.com>
2024-09-11 19:53:43 +0100

    startup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+    import "../app.css";
+  </script>
+
+<nav class='container flex justify-between mx-auto w-full text-4xl font-semibold p-4 bg-muted/40'>
+    <a>Home</a>
+    <a>Search</a>
+    <a>Login</a>
+</nav>
+
+<main class="flex flex-grow justify-center bg-muted/40 ">
+	<div class="container p-5">
+		<slot />
+	</div>
+</main>
+
+<footer class="flex justify-center bg-muted/40">
+	<div class="container flex justify-center px-6 py-2 font-semibold">
+		<h1>Copyright Alessio Grespi 2024</h1>
+		<!-- <h1>Contact: alessiogrespi@gmail.com</h1> -->
+	</div>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<div class="w-full mx-auto flex p-5 gap-x-8 h-full">
+	<div class="w-1/4 bg-neutral-50 rounded-xl">
+		<h1 class='card-title'>Subscriptions</h1>
+        <div>
+            <ul>
+                <li class='subscriptions'><a>Subscription</a></li>
+                <li class='subscriptions'><a>Subscription</a></li>
+                <li class='subscriptions'><a>Subscription</a></li>
+                <li class='subscriptions'><a>Subscription</a></li>
+                <li class='subscriptions'><a>Subscription</a></li>
+                <li class='subscriptions'><a>Subscription</a></li>
+            </ul>
+            
+        </div>
+	</div>
+	<div class="w-3/4 bg-neutral-50 rounded-xl">
+		<h1 class='card-title'>Feed</h1>
+        <div>
+            <div class='bg-neutral-100'>
+                <h3>Forum - Time</h3>
+                <h2>Post title</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis sint pariatur odio vel commodi, libero aperiam corrupti. Earum ab deserunt iusto architecto beatae nemo veniam cupiditate, libero accusamus commodi reiciendis.</p>
+                <div class='flex'>
+                    <a>Upvote/Downvote</a>
+                    <p>Comments</p>
+                    <p>views</p>
+                    <button>Share</button>
+                </div>
+            </div>
+        </div>
+	</div>
+	<div class="w-1/6">
+		<h1 class='card-title'>Hisotry</h1>
+	</div>
+</div>
+
+<style>
+    .card-title{
+        @apply font-semibold text-center text-2xl
+    }
+
+    .subscriptions{
+        @apply font-medium text-lg px-2 py-0.5 gap-y-2 rounded-xl hover:bg-neutral-100
+    }
+</style>
